10d791bde7c8e454dcd5e14cf994d06b6b4c2d92
komeilsholeh <21224612@student.uwl.ac.uk>
2017-03-07 19:46:42 +0000

    getfield function finished

--- SqlToMongodb/clsInsert.cs
+++ SqlToMongodb/clsInsert.cs
@@ -9,9 +9,11 @@ namespace SqlToMongodb
 {
     class clsInsert
     {
+        private string seperator = ".^arr.";
         public string getJsonCommand(string InsertCommand)
         {
             string command = "";
+            getValues(InsertCommand);
             ArrayList fields = getFields(InsertCommand);
             for (int i = 0; i < fields.Count; i++)
             {
@@ -37,57 +39,52 @@ namespace SqlToMongodb
                 switch (fieldsStr.Substring(i, 1))
                 {
                     case ")":
-                        fieldsArray.Add(fld);
+                        fieldsArray.Add(fld.Trim());
                         break;
                     case ",":
-                        fieldsArray.Add(fld);
-                        if (fld.LastIndexOf(".^arr.") == -1)
+                        fieldsArray.Add(fld.Trim());
+                        if (fld.LastIndexOf(seperator) == -1)
                         {
                             fld = "";
                         }
                         else
                         {
-                            fld = fld.Substring(0, fld.LastIndexOf(".^arr.") + 6);
+                            fld = fld.Substring(0, fld.LastIndexOf(seperator) + seperator.Length);
                         }
                         break;
                     case "[":
-                        fld = fld + ".^arr.";
+                        fld = fld + seperator;
                         break;
                     case "]":
-                        fieldsArray.Add(fld);
-                        fld = fld.Substring(0, fld.IndexOf(".^arr.")+6);
+                        fieldsArray.Add(fld.Trim());
+                        if (fld.IndexOf(seperator) == fld.LastIndexOf(seperator))
+                        {
+                            fld = "";
+                        }
+                        else
+                        {
+                            fld = fld.Substring(0, fld.IndexOf(seperator) + seperator.Length);
+                        }                        
                         i++;
                         break;
                     default:
                         fld = fld + fieldsStr.Substring(i, 1);
                         break;
-
                 }
-            }
-
-            //char[] seperator =[',', '[', ']'];
-            //string[] fieldsarray = fieldsStr.Split(seperator);
-            //if (fieldsStr.IndexOf('[') != -1)
-            //{
-            //    for(int i = 0; i < fieldsarray.Length - 1; i++)
-            //    {
-            //        int fldpos = fieldsStr.IndexOf(fieldsarray[0]);
-            //        if (fieldsStr.Substring(fldpos + fieldsarray[i].Length - 1, 1) == "[")
-            //        {
-
-
-            //        }
-            //    }
-            //}
-            
+            }            
             return fieldsArray;
 
         }
 
         private void getValues(string InsertCommand)
         {
-            int startindex = InsertCommand.ToLower().IndexOf("values");
-            string values = InsertCommand.Substring(startindex + 6 ,InsertCommand.Length-1);
+            int start = InsertCommand.ToLower().IndexOf("values") + 6;
+            int end = InsertCommand.LastIndexOf(")") + 1;
+            
+            string valuesStr = InsertCommand.Substring(start, end - start).Trim();
+
+     
+            string[] valuesArray = valuesStr.Split(',');
         }
     }
 }
--- SqlToMongodb/clsSqlToMongo.cs
+++ SqlToMongodb/clsSqlToMongo.cs
@@ -9,27 +9,27 @@ namespace SqlToMongodb
 {
     public class clsSqlToMongo
     {
-        //static void Main(string[] args)
-        //{
-        //    MongodbConnection mc = new MongodbConnection();
-        //    BsonDocument person = new BsonDocument {
-        //       { "first_name", "Steven"},
-        //       { "last_name", "Edouard"},
-        //       { "accounts", new BsonArray {
-        //           new BsonDocument {
-        //               { "account_balance", 50000000},
-        //               { "account_type", "Investment"},
-        //               { "currency", "USD"}
-        //           }
-        //       }}
-        //   };
-        //    mc.connect();
-        //    mc.collection.Insert(person);
-        //    System.Console.WriteLine(person["_id"]);
-        //    System.Console.ReadLine();
-        //}
+        static void Main(string[] args)
+        {
+            MongodbConnection mc = new MongodbConnection();
+            BsonDocument person = new BsonDocument {
+               { "first_name", "Steven"},
+               { "last_name", "Edouard"},
+               { "accounts", new BsonArray {
+                   new BsonDocument {
+                       { "account_balance", 50000000},
+                       { "account_type", "Investment"},
+                       { "currency", "USD"}
+                   }
+               }}
+           };
+            mc.connect();
+            mc.collection.Insert(person);
+            System.Console.WriteLine(person["_id"]);
+            System.Console.ReadLine();
+        }
 
-////
+        ////
         public string commandConvertor(string SQLcommand)
         {
             string[] command =SQLcommand.Split(' ');
