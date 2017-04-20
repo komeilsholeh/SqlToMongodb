a7e6d8ab2f949f88ed3004838e4e5d80d0139978
komeilsholeh <21224612@student.uwl.ac.uk>
2017-04-20 19:54:55 +0100

    new classes are added

--- SqlToMongodb/clsSqlToMongo.cs
+++ SqlToMongodb/clsSqlToMongo.cs
@@ -9,8 +9,7 @@ using System.Collections;
 namespace SqlToMongodb
 {
     public class clsSqlToMongo
-    {
-       
+    {       
         public string commandConvertor(string SQLcommand)
         {
             string[] command =SQLcommand.Split(' ');
@@ -18,26 +17,23 @@ namespace SqlToMongodb
             switch (command[0].ToLower())
             {
                 case "select":
-
+                    // calling related class and method;
                     break;
                 case "insert":
+                    // calling related class and method;
                     clsInsert ci = new clsInsert();
                     mongoCommand= ci.InsertDocument(SQLcommand);                    
                     break;
                 case "update":
-
+                    // calling related class and method;
                     break;
                 case "delete":
+                    // calling related class and method;
                     break;
                 default:
                     break;
             }
             return mongoCommand; 
         }
-
-
-
-
-
     }
 }
